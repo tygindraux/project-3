f62732aaabffdceadf72767c7ba3d813db4c297d
Tyler Gindraux <tylergindraux@gmail.com>
2015-12-28 14:56:46 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,125 @@
+// Frogger Classic Arcade project 3
+// Tyler Gindraux's modified app.js
+
+// Enemies our player must avoid
+var Enemy = function(x,y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+
+    // enemy starting location
+    this.x = x;
+    this.y = y;
+
+    //speed for enemy, picked randomly
+    this.speed = Math.floor(Math.random()*300);
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    this.x = this.x + this.speed * dt;
+
+    //when enemy leaves the game area, it will set the position to -80, re-enter
+    if(this.x > 501) {
+        this.x = -80;
+    }
+
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+var Player = function(){
+    // player image
+    this.sprite = 'images/char-princess-girl.png';
+
+    // player starting location
+    this.x = x;
+    this.y = y;
+
+    // score for the game
+    this.score = 0;
+}
+
+Player.prototype.update = function() {
+};
+
+// Draw the player on the screen, copied from enemy one
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// this is where the arrow keys are written in, to control the player
+Player.prototype.handleInput = function(key){
+    if(key == 'left'){
+        if (this.x > 0){
+            this.x -= 100;
+        }
+    }
+    else if(key == 'right'){
+        if (this.x < 400){
+            this.x += 100;
+        }
+    }
+    else if(key == 'up'){
+        if (this.y > 50){
+            this.y -= 100;
+        }
+        //win
+        else{
+            alert("YOU WON! GOOD JOB!")
+            this.score += 1;
+            player.reset();
+        }
+    }
+    else if(key == 'down'){
+        if (this.y < 400){
+            this.y +=100;
+        }
+    }
+};
+
+Player.prototype.reset = function() {
+    this.x = 100;
+    this.y = 200;
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+
+var allEnemies = [
+    new Enemy(0,63),
+    new Enemy(0,120),
+    new Enemy(0,201),
+];
+
+// Place the player object in a variable called player
+var player = new Player(150, 350);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
